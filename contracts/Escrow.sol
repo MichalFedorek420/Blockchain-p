e0a50391a236f6341369c8c6b01e584510e0f4a6
MichalFedorek420 <michal.fedorek2@gmail.com>
2024-01-22 22:02:08 +0100

    ughhh

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -74,7 +74,33 @@ contract Escrow {
         inspectionPassed[_nftID] = _passed;
     }
 
-    function approveSale(uint256)
+    function approveSale(uint256 _nftID) public {
+        approval[_nftID][msg.sender] = true;
+    }
+
+    function finalizeSale(uint256 _nftID) public {
+        require(inspectionPassed[_nftID]);
+        require(approval[_nftID][buyer[_nftID]]);
+        require(approval[_nftID][seller]);
+        require(approval[_nftID][lender]);
+        require(address(this).balance >= purchasePrice[_nftID]);
+
+        isListed[_nftID] = false;
+
+        (bool success, ) = payable(seller).call{value: address(this).balance}("");
+        require(success);
+
+        IERC721(nftAddress).transferFrom(address(this), buyer[_nftID], _nftID);
+
+    }
+
+    function cancelSale(uint256 _nftID) public {
+        if (inspectionPassed[_nftID] == false) {
+            payable(buyer[_nftID]).transfer(address(this).balance);
+        } else {
+            payable(seller).transfer(address(this).balance);
+        }
+    }
 
     receive() external payable {}
 
--- src/App.js
+++ src/App.js
@@ -15,12 +15,56 @@ import config from './config.json';
 
 function App() {
 
+  const [provider, setProvider] = useState(null)
+
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+
+    window.etherum.on('accountsChanged', async () => {
+      const accounts = await window.etherum.request({method: 'eth_requestAccounts'});
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+  }
+
+  useEffect(()=> {
+    loadBlockchainData()
+  }, [])
+
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
       <div className='cards__section'>
 
-        <h3>Welcome to Millow</h3>
+        <h3>Homes for you</h3>
+
+        <hr />
+
+        <div className = 'cards'>
+          <div className = 'card'>
+            <div className ='card__image'>
+              <img src = "" alt="Home"/>
+            </div>
+            <div className='card__info'>
+              <h4>1 Eth</h4>
+              <p>
+                <strong>1</strong>bds
+                <strong>2</strong>ba
+                <strong>3</strong>sqft
+              </p>
+              <p>420 Kielecka</p>
+            </div>
+          </div>
+        </div>
 
       </div>
 
